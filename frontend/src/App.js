1239712a7649bf573e77fe634f936edcae3b3a26
Dylan Gavin <dyl.gavin@gmail.com>
2022-12-30 17:37:55 -0800

    login/signup page nav bar changed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,22 +4,26 @@ import LoginFormPage from "./components/LoginFormPage";
 import SignupFormPage from "./components/SignupFormPage";
 import PageNotFound from './components/PageNotFound';
 import Navigation from "./components/Navigation";
+import LoginNavigation from './components/LoginNavigation';
 
 function App() {
   return (
     <>
-      <Navigation />
         <Switch>
           <Route path="/login" exact >
+            <LoginNavigation />
             <LoginFormPage />
           </Route>
           <Route path="/signup" exact>
+            <LoginNavigation />
             <SignupFormPage />
           </Route>
           <Route path="/error" exact>
+            <LoginNavigation />
             <PageNotFound />
           </Route>
           <Route path="/" exact>
+            <Navigation />
             {/* route to splash page */}
           </Route>
           <Redirect to="/error"/>
---
+++ frontend/src/components/LoginNavigation/index.js
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom"
+import stockX from "./StockX_logo2.png"
+import './LoginNavigation.css'
+
+function LoginNavigation() { 
+
+    return (
+        <nav className="login-nav">
+            <Link to="/"><img src={stockX}/></Link>
+        </nav>
+    )
+}
+
+export default LoginNavigation
