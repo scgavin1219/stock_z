3b1aa83b75c2aa35659d2a475c6fa193497d097e
Dylan Gavin <dyl.gavin@gmail.com>
2023-01-02 16:22:14 -0800

    splash page styling

--- frontend/src/components/Splash/index.js
+++ frontend/src/components/Splash/index.js
@@ -1,14 +1,27 @@
 import React from 'react'
 import './Splash.css'
 import jordan from './AJ1.png'
+import dunk from './dunk.png'
 
 function Splash() {
 
 
   return (
     <div className='splash-container'>
+        <div className='product-type'>
+          <h5>Shoes</h5>
+          <h5>Watches</h5>
+          <h5>Electronics</h5>
+          <h5>Trading Cards</h5>
+          <h5>Collectibles</h5>
+          <h5>Accessories</h5> 
+        </div>
         <div className='jordan'> 
-             <img src={jordan} alt="Air Jordan 1"/>
+             <img src={dunk} alt="Air Jordan 1"/>
+        </div>
+        <div className='products'>
+          <h1>All of the products will go here</h1>
+          <h2>Not sure, but maybe sort by category</h2>
         </div>
     </div>
   )
