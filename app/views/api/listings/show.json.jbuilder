0a7ff65a2939a77497b01785d5cbab6ab9e25fa8
Dylan Gavin <dyl.gavin@gmail.com>
2023-01-04 09:35:52 -0800

    show page styling

---
+++ app/views/api/listings/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! listing, :id, :name, :brand, :style, :colorway, :description, :old_price, :price, :category
--- frontend/src/components/ListingShow/index.js
+++ frontend/src/components/ListingShow/index.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getListing, fetchListing } from '../../store/listings'
 import { useParams } from 'react-router-dom'
+import './ListingShow.css'
 
 function ListingShow() {
     const dispatch = useDispatch()
@@ -16,18 +17,22 @@ function ListingShow() {
 
 
   return (
-    <div className='show-container'>
-        <h1>{listing.name}</h1>
-        <div className='show-pic'>
-            <p>pic goes here</p>
+    <div id="show" className='show-container'>
+        <div className='show-title'>
+            <h1>{listing.name}</h1>
         </div>
-        <div className='show-information'>
-            <ul>
-                <li>{listing.description}</li>
-                <li>{listing.style}</li>
-                <li>{listing.colorway}</li>
-                <li>{listing.price}</li>
-            </ul>
+        <div className='show-bottom'>
+            <div className='show-pic'>
+                <p>pic goes here</p>
+            </div>
+            <div className='show-information'>
+                <ul>
+                    <li>DESCRIPTION: {listing.description}</li>
+                    <li>STYLE: {listing.style}</li>
+                    <li>COLORWAY: {listing.colorway}</li>
+                    <li>PRICE: {listing.price}</li>
+                </ul>
+            </div>
         </div>
     </div>
   )
