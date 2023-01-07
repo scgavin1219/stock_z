2a3a898864924cf87c0027948f18d7d3742a7314
Dylan Gavin <dyl.gavin@gmail.com>
2023-01-07 11:43:04 -0800

    validations for review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,7 @@
 class Review < ApplicationRecord
-    # validates :user_id, :listing_id, :rating, :text_rating, presence: true
+    validates :user_id, :listing_id, :rating, :text_rating, presence: true
+    validates :rating, numericality: { only_integer: true, in: 0..5 }
+    validates :text_rating, length: { in: 20..500 }
 
     belongs_to :user
     belongs_to :listing 
