9cd8d09810bb48879a19ce2efaa0df88804fb8b5
Dylan Gavin <dyl.gavin@gmail.com>
2023-01-10 09:51:14 -0800

    img carousel on splash page

--- frontend/src/components/Splash/index.js
+++ frontend/src/components/Splash/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './Splash.css'
 import { Link } from 'react-router-dom'
 // import jordan from './AJ1.png'
@@ -7,9 +7,22 @@ import babe from './bape.png'
 import trends from './trends.png'
 import jshoes from './js.png'
 import Carousel from './Carousel'
+import { useState } from 'react'
 
 function Splash() {
-  const slides = [dunk, babe, trends, jshoes] 
+  const slide = [babe, trends, jshoes] 
+  const [currentIdx, setCurrentIdx] = useState(0)
+
+  useEffect(()=> { 
+    const splashInterval = setInterval(()=> { 
+      if (currentIdx < slide.length - 1) { 
+        setCurrentIdx(currentIdx + 1)
+      } else { 
+        setCurrentIdx(0)
+      }
+    }, 4000) 
+    return () => clearInterval(splashInterval)
+  }, [currentIdx])
 
   return (
     <div className='splash-container'>
@@ -23,10 +36,11 @@ function Splash() {
         </div>
         <div className='jordan'> 
             
-            <img src={dunk} alt="Air Jordan 1"/>
+            {/* <img src={dunk} alt="Air Jordan 1"/> */}
         </div>
-        <div className='jordan-two'>
+        <div className='splash-img'>
           {/* <Carousel /> */}
+          <img src={slide[currentIdx]} id="splash-img" />
         </div>
     </div>
   )
