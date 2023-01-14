7b585940730d609dfe679ea532b00b87c364192d
Dylan Gavin <dyl.gavin@gmail.com>
2023-01-14 14:11:40 -0800

    fixed about me page

--- frontend/src/components/About/index.js
+++ frontend/src/components/About/index.js
@@ -23,13 +23,14 @@ function About() {
   return (
     <div className="about-container">
         <div className='about-picture'>
-            <img id="spiderman" src={slides[currentIdx]} alt="spiderman" />
+            <img id="spiderman" src={spidey2} alt="spiderman" />
+            <img id="spiderman" src={spidey1} alt="spiderman" />
         </div>
         <div className='about-bio'>
             <div className='about-title'>
                 <h1> Dylan Gavin</h1>
             </div>
-            <p>I am a former teacher turned Software Engineer. 
+            <p>My name is Dylan Gavin and I am a former teacher turned Software Engineer. 
                 Thank you so much for visiting my StockX clone. 
                 As a teacher I would often dress
                 up for my students on Halloween as some of my favorite
@@ -38,11 +39,17 @@ function About() {
                 me to StockX and is the inspiration behind this project. 
             </p>
             <p>
+                My stockX clone is built with a front end that is coded in JavaScript.
+                I used React and Redux to render components and to manage state. I used 
+                a Ruby on Rails backend and a PostgreSQL database. This websited has three
+                full CRUD features. Users that are signed in are able to create, update, and
+                delete both reviews and their shopping carts. Users are also able to add
+                products to their favorites and any user can search by name, category, or 
+                description.
+                </p> 
+                <p> 
                 If you have any questions about the project I would love
-                to connect, my socials are on the bottom of the page.  This
-                 project is built on a Rails backend and a React frontend. 
-                My other interests....
-                IDK THIS IS ALL HOT GARBAGE AND I'M TIRED
+                to connect, my socials are on the bottom of the page. 
             </p>
         </div>
     
